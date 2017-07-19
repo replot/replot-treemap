4de620ab92d252b9dc391a07dab8f1ae958623a0
Matt Farejowicz <mfarejowicz@gmail.com>
2017-07-19 14:41:17 -0400

    clarify naming

--- examples/example.jsx
+++ examples/example.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import TreeMapManager from "../src/index.jsx"
+import TreeMap from "../src/index.jsx"
 
 
 class TreeDataRow extends React.Component {
@@ -91,34 +91,35 @@ class ExampleApp extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      // data: [
-      //   {population: 650, country: "China", state: "Beijing", city: "Miyun"},
-      //   {population: 902, country: "China", state: "Beijing", city: "Tongzhou"},
-      //   {population: 120, country: "China", state: "Beijing", city: "Yizhuang"},
-      //   {population: 800, country: "United States", state: "California", city: "San Francisco"},
-      //   {population: 10020, country: "United States", state: "California", city: "Los Angeles"},
-      //   {population: 150, country: "United States", state: "Vermont", city: "Newport"},
-      //   {population: 20, country: "United States", state: "Vermont", city: "Montpelier"},
-      //   {population: 202, country: "United States", state: "Illinois", city: "Chicago"},
-      //   {population: 112, country: "Canada", state: "Ontario", city: "Kingston"},
-      //   {population: 80, country: "Canada", state: "Ontario", city: "Barrie"},
-      // ],
-      // keyOrder: ["country", "state", "city"]
+      data: [
+        {population: 650, country: "China", state: "Beijing", city: "Miyun"},
+        {population: 902, country: "China", state: "Beijing", city: "Tongzhou"},
+        {population: 120, country: "China", state: "Beijing", city: "Yizhuang"},
+        {population: 800, country: "United States", state: "California", city: "San Francisco"},
+        {population: 10020, country: "United States", state: "California", city: "Los Angeles"},
+        {population: 150, country: "United States", state: "Vermont", city: "Newport"},
+        {population: 20, country: "United States", state: "Vermont", city: "Montpelier"},
+        {population: 202, country: "United States", state: "Illinois", city: "Chicago"},
+        {population: 112, country: "Canada", state: "Ontario", city: "Kingston"},
+        {population: 80, country: "Canada", state: "Ontario", city: "Barrie"},
+      ],
+      keyOrder: ["country", "state", "city"]
+      //Other test debugging datasets
       // data: [
       //   {country: "China", population: 1388232693},
       //   {country: "India", population: 1342512706},
       //   {country: "USA", population: 326474013}
       // ]
-      data: [
-        {country: "Earth", population: 800000},
-        {country: "Mars", population: 98765},
-        {country: "China", population: 9000},
-        {country: "Russia", population: 10000},
-        {country: "India", population: 83},
-        {country: "USA", population: 100},
-        {country: "Iceland", population: 2},
-        {country: "Greenland", population: 1}
-      ]
+      // data: [
+      //   {country: "Earth", population: 800000},
+      //   {country: "Mars", population: 98765},
+      //   {country: "China", population: 9000},
+      //   {country: "Russia", population: 10000},
+      //   {country: "India", population: 83},
+      //   {country: "USA", population: 100},
+      //   {country: "Iceland", population: 2},
+      //   {country: "Greenland", population: 1}
+      // ]
       // data: [
       //   {planet: "Earth", country: "USA", population: 40000},
       //   {planet: "Earth", country: "China", population: 200},
@@ -160,9 +161,8 @@ class ExampleApp extends React.Component {
         <h1 style={{textAlign: "center"}}> Treemaps for Replot </h1>
         <TreeDataTable data={this.state.data} updateData={this.updateData.bind(this)} />
         <div style={{width:"70%", display:"inline-block"}}>
-          <TreeMapManager data={this.state.data} weightKey="population"
-            titleKey="country" tooltip
-            tooltipContents={this.fillTooltip.bind(this)}/>
+          <TreeMap data={this.state.data} weightKey="population"
+            keyOrder={this.state.keyOrder} width="50%" />
         </div>
       </div>
     )
